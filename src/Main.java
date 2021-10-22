2067ab2f5f408bfb37fbe484011c7f11bafcc115
Zende-Magloire <89619545+Zende-Magloire@users.noreply.github.com>
2021-10-22 14:53:21 -0400

    day 3

--- src/Main.java
+++ src/Main.java
@@ -24,26 +24,27 @@ public class Main {
             if (choice == 3)
                 return;
             // ask the user which shape index they are interested in
-            System.out.print("Which shape would you like to select?");
+            System.out.print("Which shape would you like to select?  ");
             // read user input for shape selection
              int sChoice = Integer.valueOf(Reader.nextLine());
             // switch on user menu selection
             switch (choice) {
-                case 1:
+                case 1: {
                     //      call the selected shape's area() function
-
-                /*if ( p instanceof Circle)
-                   double area = p.area();
-                else if ( p instanceof Rectangle)
-                    Rectangle c = (Rectangle) p;
-                    area = c.area();
+                    double area = shapes[sChoice].area();
                     //      print out the area
-                    System.out.println(area);*/
-
-                case 2:
+                    System.out.println("The area is " + area);
+                    break;
+                }
+                case 2: {
                     //      call the selected shape's perimeter function
-
+                    double perimeter = shapes[sChoice].area();
                     //      print out the perimeter
+                    System.out.println("The perimeter is " + perimeter);
+                    break;
+                }
+                default:
+                    break;
 
             }
         }
@@ -53,49 +54,49 @@ public class Main {
         // iterate through the array of shapes passed in with a definite loop
         for (int i = 0; i < stuff.length; i++) {
             //    ask the user if this shape should be a circle
-            System.out.println("Is the shape a circle? (yes = 1, no = 2)");
+            System.out.print("What kind of shape is it? (circle = 1, rectangle = 2)  ");
             //    read their response
             int ans = Integer.valueOf(Reader.nextLine());
-              if (ans > 2)
-              return;
+            if (ans > 2)
+                return;
             //    if they want a circle
             //       ask and read in x, y, and radius values
             if (ans == 1){
-                System.out.println("Enter x coordinate (positive)");
-            double xC = Double.valueOf(Reader.nextLine());
-                System.out.println("Enter y coordinate (positive)");
-            double yC = Double.valueOf(Reader.nextLine());
-                System.out.println("Enter the radius (positive)");
-            double radius = Double.valueOf(Reader.nextLine());
+                System.out.print("Enter x coordinate (positive):  ");
+                double xC = Double.valueOf(Reader.nextLine());
+                System.out.print("Enter y coordinate (positive):  ");
+                double yC = Double.valueOf(Reader.nextLine());
+                System.out.print("Enter the radius (positive):  ");
+                double radius = Double.valueOf(Reader.nextLine());
             //       create an instance of a Circle obj and store it in the shape array passed in
-             stuff[i]= new Circle(xC, yC, radius);
+                stuff[i]= new Circle(xC, yC, radius);
             }
-               else if (ans == 2){
+            else if (ans == 2){
             //       ask and read in x, y, width and height values
-                System.out.println("Enter x coordinate (positive)");
-            double xR = Double.valueOf(Reader.nextLine());
-                System.out.println("Enter y coordinate (positive)");
-            double yR = Double.valueOf(Reader.nextLine());
-                System.out.println("Enter the width (positive)");
-            double wR = Double.valueOf(Reader.nextLine());
-                System.out.println("Enter the length (positive)");
-            double lR = Double.valueOf(Reader.nextLine());
+                System.out.print("Enter x coordinate (positive):  ");
+                double xR = Double.valueOf(Reader.nextLine());
+                System.out.print("Enter y coordinate (positive):  ");
+                double yR = Double.valueOf(Reader.nextLine());
+                System.out.print("Enter the width (positive):  ");
+                double wR = Double.valueOf(Reader.nextLine());
+                System.out.print("Enter the length (positive):  ");
+                double lR = Double.valueOf(Reader.nextLine());
             //       create an instance of a Rectangle obj and store it in the shape array passed in
              stuff[i] = new Rectangle(xR, yR, wR, lR);}
         }
     }
 
 
-        private static void printShapes (Shape[]stuff) {
-            System.out.println("\n--------------------");
+    private static void printShapes (Shape[]stuff) {
+        System.out.println("\n--------------------");
 
-            // iterate through the array of shapes passed in with a definite loop
-            for (int i = 0; i < stuff.length; i++) {
-                //    print each index and shape all on one line
-                System.out.println( i + ") " + stuff[i]);
-            }
-
+        // iterate through the array of shapes passed in with a definite loop
+        for (int i = 0; i < stuff.length; i++) {
+            //    print each index and shape all on one line
+            System.out.println( i + ") " + stuff[i]);
         }
+
+    }
 
         private static void printMenu ()
         {
